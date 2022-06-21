247453f799afc19f8d7ed9fe2c7e5dc10be644d9
Sebastien Mestrallet <sebastien.mestrallet@cea.fr>
2022-06-21 11:15:57 +0200

    add collection parser

--- apps/test.cpp
+++ apps/test.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 
 #include "paths.h"
+#include "collections.h"
 
 int main(int argc, char *argv[]) {
     paths p;
     std::cout << "shared_data = " << p.shared_data() << std::endl;
+
+    std::set<std::filesystem::path> input_folders, subcollections;
+    if(expand_collection(p.shared_data() / "MAMBO_CAD.txt",input_folders,subcollections)) {
+        //an error occured
+        return 1;
+    }
+
+    std::cout << "Set of input folders (" << input_folders.size() << " elements):" << std::endl;
+    for(auto& p : input_folders) {
+        std::cout << p.string() << std::endl;
+    }
 
     return 0;
 }
---
+++ include/collections.h
@@ -0,0 +1,108 @@
+#pragma once
+
+#include <iostream>
+#include <filesystem>
+#include <set>
+
+#define SET_CONTAINS(set,value) ((set).find(value) != (set).end())
+
+//count the number of folders +1 between a file/folder and the root
+int get_depth(std::filesystem::path p) {
+    if(p == p.parent_path()) {
+        //we reached the root path
+        return 0;
+    }
+    else if (p.filename()=="")
+    {
+        //even though the parent path of /var/tmp/ is /var/tmp, consider they have the same depth
+        return get_depth(p.parent_path());
+    }
+    else {
+        return get_depth(p.parent_path())+1;
+    }
+}
+
+//'collection' is the path to a .txt collection file (case multiple entries), or to a folder (case unique entry)
+//'entries' will list all the folders that 'collection' contains
+//'subcollections' ensure there is no cyclic inclusion
+//'depth' ensure all the folders have the same depth (distance to root)
+//return 1 if an error occured
+bool expand_collection(const std::filesystem::path& collection, std::set<std::filesystem::path>& entries, std::set<std::filesystem::path>& subcollections, int depth=-1) {
+
+    if(!std::filesystem::exists(collection)) {
+        std::cerr << "Error : " << collection.string() << " doesn't exist" << std::endl;
+        return 1;
+    }
+
+    if(std::filesystem::is_directory(collection)) {
+        //when expand_collection() is called recursively, 'collection' is ensured to be a .txt file
+        //but the top level call of expand_collection() could recieve a folder,
+        //which means the user only chose one entry
+        entries.emplace(collection);
+        return 0;
+    }
+    else if(collection.extension()!=".txt") {
+        std::cerr << "Error : " << collection.string() << " is neither a .txt file nor a folder" << std::endl;
+        return 1;
+    }
+    
+    std::ifstream input_file(collection);
+    std::string line;
+    std::filesystem::path new_entry;
+    int new_entry_depth;
+    if (!input_file.is_open()) {
+        std::cerr << "Error : could not open " + collection.string() << std::endl;
+        return 0;
+    }
+    
+    while (std::getline(input_file,line)) { //read line by line
+
+        if(line[0] == '#') { //ignore lines starting with '#'
+            continue;
+        }
+
+        new_entry = collection.parent_path() / line;
+
+        //a collection can only contains directories (the entries) or .txt files (subcollections)
+
+        if(std::filesystem::is_directory(new_entry)) {
+            //check if this new entry is of same depth as the others
+            new_entry_depth = get_depth(new_entry);
+            if(depth == -1) { //if depth is undefined (case of the first entry found)
+                depth = new_entry_depth;//store the depth ("distance" to root)
+            }
+            else if(new_entry_depth != depth) {//if the depth of this entry is different
+                std::cerr << "Error : the depth of " << line << " (in " << collection.string() << ") is different from the previous entries" << std::endl;
+                return 1;
+            }
+            else {
+                entries.emplace(new_entry);
+            }
+        }
+        else if(std::filesystem::is_regular_file(new_entry)) {
+            if(new_entry.extension()==".txt") {
+                //to avoid cyclic inclusion of collections, i.e. a.txt containing b.txt containing a.txt,
+                //store the subcollections found in a set, and ignore already-opened collections
+                if(SET_CONTAINS(subcollections,new_entry)) {
+                    std::cout << "Info : " << collection.string() << " has already been opened and will be skipped" << std::endl;
+                    continue;//jump to next line
+                }
+                subcollections.emplace(collection);
+                if(expand_collection(new_entry,entries,subcollections,depth)) {
+                    return 1;
+                }
+                //else the recursive call of expand_collection() didn't encountered any issue -> continue reading
+            }
+            else {
+                std::cerr << "Error : " << line << " (in " << collection.string() << ") is not a .txt file" << std::endl;
+                return 1;
+            }
+        }
+        else {
+            std::cerr << "Error : " << line << " (in " << collection.string() << ") isn't a valid file" << std::endl;
+            return 1;
+        }
+    }
+
+    return 0;
+}
