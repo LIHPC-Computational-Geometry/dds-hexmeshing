8456b90a0c8f3a62c7b1c9bf58935e516d3f24a0
Sebastien Mestrallet <sebastien.mestrallet@hey.com>
2023-03-31 16:59:15 +0200

    add function to retrieve dbpath of MongoDB

--- HexMeshWorkshop.py
+++ HexMeshWorkshop.py
@@ -1,8 +1,34 @@
 from pymongo import MongoClient
 from logging import *
 from pathlib import Path
+import subprocess
 
 getLogger().setLevel(INFO)
+
+def get_MongoDB_dbpath():
+    """
+    Get the path to the data location used by MongoDB
+
+    Return the custom path if `mongod` was called with `--dbpath`,
+    else retuns the default path `/data/db/`
+    """
+    # it seems there is no easy way to get this value
+    # https://stackoverflow.com/questions/7247474/how-can-i-tell-where-mongodb-is-storing-data-its-not-in-the-default-data-db
+    # This function will not work
+    # - if there are several mongod processes
+    # - if the OS is not linux
+    # - if the default data location was modified in the MongoDB configuration
+    process = subprocess.run('ps -xa | grep mongod', shell=True, capture_output=True).stdout.decode().splitlines()[0] # only consider the first line
+    index = process.find('--dbpath ')
+    if index==-1:
+        fatal('mongod is not running')
+        fatal('Cannot retrieve dbpath value')
+        exit(1)
+    line = process[index+9:] # get what is after '--dbpath '
+    line = line.split(' ')[0] # get what is between '--dbpath ' and the next space
+    if len(line)==0:
+        return Path('/data/db/') # mongod was called without '--dbpath ', so it uses the default data path
+    return Path(line)
 
 class HexMeshWorkshopDatabase:
     """
@@ -11,6 +37,7 @@ class HexMeshWorkshopDatabase:
     def __init__(self,host,port):
 
         self.client = MongoClient(host,port)
+        self.dbpath = get_MongoDB_dbpath()
 
         if 'HexMeshWorkshop' not in self.client.list_database_names():
             warning('The MongoDB instance at host=' + host + ' port=' + str(port) + ' has no database named \'HexMeshWorkshop\', it will be created')
@@ -34,11 +61,11 @@ class HexMeshWorkshopDatabase:
             if not input.is_dir():
                 fatal(str(input) + ' is not a folder')
                 exit(1)
-
+        
         for subfolder in [x for x in input.iterdir() if x.is_dir()]:
-            if subfolder.name == "Scripts":
+            if subfolder.name == 'Scripts':
                 continue # ignore this subfolder
-            for file in [x for x in subfolder.iterdir() if x.suffix == ".step"]:
+            for file in [x for x in subfolder.iterdir() if x.suffix == '.step']:
                 print('found ' + file.name)
 
         if input==None:
