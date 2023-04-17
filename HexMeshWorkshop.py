9fbf7d333313816f21f0a4bfe24f2d4405ef9780
Sebastien Mestrallet <sebastien.mestrallet@hey.com>
2023-04-17 09:49:24 +0200

    database insertion of MAMBO step files

--- HexMeshWorkshop.py
+++ HexMeshWorkshop.py
@@ -5,6 +5,8 @@ import subprocess
 from shutil import copyfile, rmtree
 from os import mkdir, path
 from json import load
+from abc import ABC, abstractmethod
+from collections import Counter
 
 getLogger().setLevel(INFO)
 
@@ -75,6 +77,40 @@ class UserInput():
         while user_choice not in ['y','yes','n','no']:
             user_choice = input(question + ' [yes/no] ').lower()
         return ((user_choice[0] == "y"))
+    
+class AbstractDocument(ABC):
+
+    @abstractmethod #prevent user from instanciating an AbstractDocument
+    def __init__(self):
+        self.parent = None
+        self.children = dict()
+
+    def type(self) -> str:
+        return self.__class__.__name__
+    
+    @abstractmethod
+    def print_parent(self):
+        pass
+
+    @abstractmethod
+    def print_children(self):
+        pass
+
+class step(AbstractDocument):
+    """
+    Interface to a step folder
+    """
+
+    # Necessary files
+    # - CAD.step
+    # Optionnal files
+    # - thumbnail.png
+
+    # TODO only 2 ways to init
+    # - from a path : check if a document has this path in the step collection, and fetch its data
+    # - from an ObjectID : check if this ObjectID is a document in the step colleciton, and fetch its data
+    def __init__(self,path: Path):
+        AbstractDocument.__init__(self)
 
 class HexMeshWorkshopDatabase:
     """
@@ -104,6 +140,73 @@ class HexMeshWorkshopDatabase:
         self.client.drop_database(self.db)
         self.db = self.client.HexMeshWorkshop # new empty database
 
+    def count_in_all_collections(self,filter):
+        count = 0
+        for collection in self.db.list_collection_names():
+            cursor = self.db[collection].find(filter)
+            count += len(list(cursor))
+        return count
+
+    def insert(self,type: type,**kwargs):
+        """
+        Insertion of a document inside the HexMeshWorkshop database.
+
+        This function is ensuring:
+        - there is no other document, in all collecitions, with the same path
+        - target files do not already exist in the data folder
+
+        Returns the object ID if the document was successfully added, else None
+        """
+
+        if not issubclass(type,AbstractDocument):
+            error('class \'' + type.__name__ + '\' is not a subclass of \'AbstractDocument\'')
+            error('so an instance of this class cannot be inserted in an HexMeshWorkshopDatabase')
+            return None
+
+        document_to_insert = None
+        if type == step:
+            if Counter(kwargs.keys()) != Counter(['name','source_step_file']): # unordered list comparison
+                error('Invalid argument names for a step insertion')
+                return None
+            
+            new_path = self.dbpath / kwargs['name']
+            new_path_relative = new_path.relative_to(self.dbpath)
+
+            if new_path.exists() & new_path.is_dir() :
+                # TODO allow overwriting
+                error('There already is a folder \'' + kwargs['name'] + '\' in the database files')
+                return None
+
+            # if self.db[type.__name__].find_one({"path": str(new_path_relative)}):
+            #     error('There already is a document with path \'' + str(new_path_relative) + '\' in the step collection')
+            #     return None
+
+            if self.count_in_all_collections({"path": str(new_path_relative)}) != 0:
+                # TODO allow overwriting
+                error('There already is a document with \'' + str(new_path_relative) + '\' as path in the database')
+                return None
+
+            # create the folder and move the file(s)
+            mkdir(new_path) # create a directory with the name of the step file
+            copyfile(kwargs['source_step_file'], new_path / 'CAD.step') # copy and rename the step file
+
+            # assemble the document to insert
+            document_to_insert = {
+                'path': str(new_path_relative)
+            }
+        else:
+            error('Unknown AbstractDocument subclass \'' + str(type) + '\' in HexMeshWorkshopDatabase.insert()')
+            error('You must complete HexMeshWorkshopDatabase.insert() with the specific pre-insertion tests')
+            return None
+        
+        if(document_to_insert == None): # document_to_insert must have been modified since declaration
+            error('Attempt to insert \'None\' in HexMeshWorkshopDatabase.insert()')
+            return None
+
+        # effective insertion
+        result = self.db[type.__name__].insert_one(document_to_insert) # access the collection of this type and insert the document
+        return result.inserted_id # return the ID of the inserted document
+    
     def import_MAMBO(self,input=None):
         if input==None:
             if not UserInput.ask("No input was given, so the MAMBO dataset will be downloaded, are you shure you want to continue ?"):
@@ -123,21 +226,18 @@ class HexMeshWorkshopDatabase:
             if not input.is_dir():
                 fatal(str(input) + ' is not a folder')
                 exit(1)
-        overwrite_management = UserInput()
         for subfolder in [x for x in input.iterdir() if x.is_dir()]:
             if subfolder.name == 'Scripts':
                 continue # ignore this subfolder
             for file in [x for x in subfolder.iterdir() if x.suffix == '.step']:
-                if (self.dbpath / file.stem).exists():
-                    # TODO show all the content of the folder to be removed
-                    if overwrite_management.ask_and_memorize('Folder ' + file.stem + ' already exist in the data folder, do you want to overwrite it ?'):
-                        rmtree(self.dbpath / file.stem)
-                        # TODO also delete associated documentd in database
-                    else:
-                        continue # skip this folder, the user does not want to overwrite it
-                mkdir(self.dbpath / file.stem) # create a directory with the name of the step file
-                copyfile(file,self.dbpath / file.stem / 'CAD.step') # copy and rename the step file
-                info(file.name + ' imported')
+                inserted_id = self.insert(
+                    step,
+                    name=file.stem,
+                    source_step_file=file
+                )
+                if (inserted_id != None):
+                    info(file.name + ' imported')
+                # TODO create 'MAMBO_'+subfolder.name and 'MAMBO' sets
 
         if input==None:
             pass
