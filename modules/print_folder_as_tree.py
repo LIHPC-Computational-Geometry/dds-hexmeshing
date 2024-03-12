61cac6f5bd4d03f5e2fda3a91e9ef0d569bb2540
Sébastien Mestrallet <sebastien.mestrallet@hey.com>
2024-03-12 17:56:26 +0100

    batch processing script now a method

--- modules/print_folder_as_tree.py
+++ modules/print_folder_as_tree.py
@@ -2,6 +2,8 @@ from pathlib import Path
 from rich.tree import Tree
 from rich import print
 from sys import path
+import questionary
+from shutil import rmtree
 
 # Add root of HexMeshWorkshop project folder in path
 project_root = str(Path(__file__).parent.parent.absolute())
@@ -23,3 +25,17 @@ def print_folder_as_tree(folder: Path):
     for subfolder in folder_content_as_trees(folder):
         tree.add(subfolder)
     print(tree)
+
+def print_content_and_propose_to_remove(folder: Path) -> bool:
+    """
+    Return true if the user asked to remove the folder (and so the folder has been removed)
+    """
+    print_folder_as_tree(folder)
+    answser = questionary.confirm("Do you want to remove this folder ?").ask()
+    assert(type(answser) == bool)
+    if answser:
+        rmtree(folder)
+        return True
+    else:
+        # the user choosed to keep this folder
+        return False
