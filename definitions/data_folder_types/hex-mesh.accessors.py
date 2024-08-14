97cec8ab3582a1cf4ea2ca9143515a7ad0618785
Sébastien Mestrallet <sebastien.mestrallet@hey.com>
2024-08-15 00:09:33 +0200

    fix get_mesh_stats_dict() of `hex-mesh` type

--- definitions/data_folder_types/hex-mesh.accessors.py
+++ definitions/data_folder_types/hex-mesh.accessors.py
@@ -9,7 +9,7 @@ from dds import DataFolder, log
 
 def get_mesh_stats_dict(self: DataFolder) -> dict:
     assert(self.type == 'hex-mesh')
-    return load(open(self.get_file('FILENAMES.HEX_MESH_STATS_JSON',True))) # compute if missing and load the JSON file
+    return load(open(self.get_file('HEX_MESH_STATS_JSON',True))) # compute if missing and load the JSON file
 
 # add this function to the class, as method
 log.info(f"Adding get_mesh_stats_dict() as DataFolder method for when type == 'hex-mesh'")
