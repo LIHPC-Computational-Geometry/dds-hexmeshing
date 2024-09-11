96bcc9d1387e1f431f867307025b22f21c105e58
Sébastien Mestrallet <sebastien.mestrallet@hey.com>
2024-09-11 22:41:15 +0200

    similarity computation between 2 labelings

--- definitions/data_folder_types/labeling.accessors.py
+++ definitions/data_folder_types/labeling.accessors.py
@@ -21,6 +21,30 @@ def nb_turning_points(self: DataFolder) -> int:
     stats = self.get_labeling_stats_dict()
     return int(stats['turning-points']['nb'])
 
+def compute_labeling_similarity_with(self: DataFolder, comparand: DataFolder) -> float:
+    # similar to https://github.com/LIHPC-Computational-Geometry/evocube/blob/master/src/flagging_utils.cpp#L157
+    assert(self.type == 'labeling')
+    assert(comparand.type == 'labeling')
+    self_labeling = list()
+    with open(self.get_file('SURFACE_LABELING_TXT',True),'r') as file:
+        while line := file.readline():
+            label = int(line.rstrip())
+            assert(label in range(6)) # in [0:5]
+            self_labeling.append(label)
+    nb_labels = len(self_labeling)
+    same_label_counter = 0
+    line_counter = 0
+    with open(comparand.get_file('SURFACE_LABELING_TXT',True),'r') as file:
+        while line := file.readline():
+            assert(line_counter < nb_labels) # else the comparand has more lines than self
+            label = int(line.rstrip())
+            assert(label in range(6)) # in [0:5]
+            if label == self_labeling[line_counter]:
+                same_label_counter += 1
+            line_counter += 1
+    assert(line_counter == nb_labels) # else the comparand has less lines than self
+    return float(same_label_counter) / float(nb_labels)
+
 # add these functions to the class, as methods
 log.info(f"Adding get_labeling_stats_dict() as DataFolder method for when type == 'labeling'")
 DataFolder.get_labeling_stats_dict = get_labeling_stats_dict
@@ -28,3 +52,5 @@ log.info(f"Adding has_valid_labeling() as DataFolder method for when type == 'la
 DataFolder.has_valid_labeling = has_valid_labeling
 log.info(f"Adding nb_turning_points() as DataFolder method for when type == 'labeling'")
 DataFolder.nb_turning_points = nb_turning_points
+log.info(f"Adding compute_labeling_similarity_with() as DataFolder method for when type == 'labeling'")
+DataFolder.compute_labeling_similarity_with = compute_labeling_similarity_with
