dbf8873b5b2dfbf3a43dd9905bab9533601be141
Sébastien Mestrallet <sebastien.mestrallet@hey.com>
2024-09-20 23:37:57 +0200

    colored output for `tet_meshes_stats` algo

--- definitions/algorithms/tet_meshes_stats.py
+++ definitions/algorithms/tet_meshes_stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # parse `input_folder` / 'MAMBO' / <'step' data folders> / 'Gmsh_0.1' or 'Gmsh_0.15'
+# as well as `input_folder` / 'OctreeMeshing' / 'cad' / <'tet-mesh' data folders>
 # and print min & max number of tetrahedra
 
 from dds import *
@@ -14,14 +15,16 @@ def main(input_folder: Path, arguments: list):
         logging.fatal(f'tet_meshes_stats does not need other arguments than the input folder, but {arguments} were provided')
         exit(1)
 
-    print(f"Parsing {input_folder}/MAMBO/<'step' data folders>/{GMSH_OUPUT_FOLDER_NAME}")
-    print(f"and {input_folder}/OctreeMeshing/cad")
-    print('---------')
+    console = Console(theme=Theme(inherit=False))
+
+    console.print(f"Parsing [cyan]{collapseuser(input_folder)}/MAMBO/<'step' data folders>/{GMSH_OUPUT_FOLDER_NAME}[/]")
+    console.print(f"and [cyan]{collapseuser(input_folder)}/OctreeMeshing/cad/<'tet-mesh' data folders>[/]")
+    console.rule(characters='·')
 
     min_nb_tetrahedra: Optional[int] = None
-    min_nb_tetrahedra_location = ''
+    min_nb_tetrahedra_location = Path()
     max_nb_tetrahedra: Optional[int] = None
-    max_nb_tetrahedra_location = ''
+    max_nb_tetrahedra_location = Path()
 
     for tet_mesh_path in [(x / GMSH_OUPUT_FOLDER_NAME) for x in get_subfolders_of_type(input_folder / 'MAMBO', 'step') if (x / GMSH_OUPUT_FOLDER_NAME).exists()] \
     + get_subfolders_of_type(input_folder / 'OctreeMeshing' / 'cad', 'tet-mesh'):
@@ -31,7 +34,7 @@ def main(input_folder: Path, arguments: list):
         nb_tetrahedra = tet_folder.get_tet_mesh_stats_dict()['cells']['nb'] # see definitions/algorithms/tet-mesh.accessors.py
 
         if nb_tetrahedra >= 300000:
-            print(f'{tet_folder.path} has 300k or more tetrahedra ({nb_tetrahedra})')
+            console.print(f'[cyan]{collapseuser(tet_folder.path)}[/] has 300k or more tetrahedra ({nb_tetrahedra})')
 
         if min_nb_tetrahedra is None or nb_tetrahedra < min_nb_tetrahedra:
             min_nb_tetrahedra = nb_tetrahedra
@@ -41,6 +44,6 @@ def main(input_folder: Path, arguments: list):
             max_nb_tetrahedra = nb_tetrahedra
             max_nb_tetrahedra_location = tet_folder.path
 
-    print('---------')
-    print(f'min = {min_nb_tetrahedra} tetrahedra at {min_nb_tetrahedra_location}')
-    print(f'max = {max_nb_tetrahedra} tetrahedra at {max_nb_tetrahedra_location}')
+    console.rule(characters='·')
+    console.print(f'min = [green]{min_nb_tetrahedra}[/] tetrahedra at [cyan]{collapseuser(min_nb_tetrahedra_location)}[/]')
+    console.print(f'max = [green]{max_nb_tetrahedra}[/] tetrahedra at [cyan]{collapseuser(max_nb_tetrahedra_location)}[/]')
